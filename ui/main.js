d15e96d5a9ea03fdfcf92777e8285646f414d35b
vigneshjaisankar <30869650+vigneshjaisankar@users.noreply.github.com>
2017-08-25 20:26:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,14 +5,5 @@ element.innerHTML='new value';
 
 //moving picture
 
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,100);
-};
+var img=document.getElementById('pp');
+var marginLeft=100;
