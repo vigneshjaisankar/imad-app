5f0bd9292f0c4155ecb1c664f78c87f3ec9ab97b
vigneshjaisankar <30869650+vigneshjaisankar@users.noreply.github.com>
2017-08-27 07:57:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,16 @@
-console.log('Loaded!');
-//changing the text
+var button=document.getElementById('counter');
+var counter=0;
+button.onclik=function(){
+    //makin req to counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in correct spam
+    counter=counter + 1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+}
 
 
-//moving picture
 
 
