ee62e425efaab2d6688addcf65a033b4de30a9f8
vigneshjaisankar <30869650+vigneshjaisankar@users.noreply.github.com>
2017-08-27 09:29:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ button.onclick=function(){
     //capture the response and store it in a variable
     request.onreadystatechange = function()
     {
-        if(request.readystate===XMLHttpRequest.DONE)
+        if(request.readyState===XMLHttpRequest.DONE)
         {
         //take soem action based on the request
         if(request.status===200)
